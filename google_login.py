f3ab78a669949c14816a2f2cef9c4b81189b7635
Henrique Bissoli <emp.shad@gmail.com>
2016-01-07 16:13:53 -0200

    Update google_login.py

--- google_login.py
+++ google_login.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #  -*- coding: utf-8 -*-
-import mechanize, sys, json
+import mechanize, sys, json, re
 
 def doLoginGoogle(user,passw):
 	print "\033[92m[Creating Google Session]\033[0m"
@@ -32,7 +32,7 @@ def doLoginGoogle(user,passw):
 		# My paste: u/2/folders/0B2qbDxOlHLfPeThCYUFNQlQ0Wk0 or my-drive
 		br.open("https://drive.google.com/drive/u/2/folders/0B2qbDxOlHLfPeThCYUFNQlQ0Wk0")
 		token		= br.response().read().split(',[["xsrf","')[1].split('",["')[0]
-		itens		= br.response().read().split("<script>window['_DRIVE_ivd'] = '")[1].split('\n'+"'"+';')[0]
+		itens		= str(br.response().read().split("window['_DRIVE_ivd'] = '")[1]).replace('\n', '').split(";")[0]
 		
 		#br.open("https://drive.google.com/act", '{docId:"0B2qbDxOlHLfPdEplb1k3WnYyUTA",authuser:0,minResultCount:20,recursive:true,token:'+token+'}')
 		print itens
@@ -40,10 +40,6 @@ def doLoginGoogle(user,passw):
 		# List 2
 		#br.open("https://clients6.google.com/drive/v2internal/files/0B6b4BiW-o9hxRlBnRVFlZzgwRVE?fields=kind%2Ctitle%2CmimeType%2CcreatedDate%2CmodifiedDate%2CmodifiedByMeDate%2ClastViewedByMeDate%2CfileSize%2ClastModifyingUser(kind%2C%20displayName%2C%20picture%2C%20permissionId%2C%20emailAddress)%2ChasThumbnail%2Cid%2Cshared%2CsharedWithMeDate%2CuserPermission(role)%2CexplicitlyTrashed%2CquotaBytesUsed%2Cshareable%2Ccopyable%2Csubscribed%2CfolderColor%2ChasChildFolders%2Cpermissions(kind%2Cid%2Cname%2CemailAddress%2Cdomain%2Crole%2Ctype%2CphotoLink%2CadditionalRoles%2CwithLink)%2CfileExtension%2CprimarySyncParentId%2CsharingUser(kind%2CdisplayName%2Cpicture%2CpermissionId%2CemailAddress)%2CflaggedForAbuse%2CfolderFeatures%2Cspaces%2CsourceAppId%2Crecency%2CrecencyReason%2Cparents(id)%2Clabels(starred%2Chidden%2Ctrashed%2Crestricted%2Cviewed)%2Cowners(permissionId%2CdisplayName%2Cpicture%2Cdomain%2Ckind)&openDrive=true&reason=102&syncType=0&errorRecovery=false&key="+token)
 		
-		
-		
-		# List files with paste ID
-		#
 		
 		# Upload file
 		#br.open("https://drive.google.com/upload/resumableuploadsc?authuser=0", '{"protocolVersion":"0.8","createSessionRequest":{"fields":[{"external":{"name":"file","filename":"Seguro Fian\u00e7a - Ficha Pessoa Fisica - RESIDENCIAL.pdf","put":{},"size":188113}},{"inlined":{"name":"parentId","content":"0B6b4BiW-o9hxRlBnRVFlZzgwRVE","contentType":"text/plain"}},{"inlined":{"name":"driveSourceClientService","content":"UploadWeb","contentType":"text/plain"}},{"inlined":{"name":"modifiedTime","content":"1452096907160","contentType":"text/plain"}}]}}')
