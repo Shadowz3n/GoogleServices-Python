a6fd77739edad4df0877929d33ffa0dd80706d82
Henrique Bissoli <emp.shad@gmail.com>
2016-01-11 12:01:59 -0200

    Update google_login.py

--- google_login.py
+++ google_login.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 #  -*- coding: utf-8 -*-
-import mechanize, sys
+# Google Services (IMPS)
+# @autor: Henrique Bissoli Silva (emp.shad@gmail.com)
+# Updates: https://github.com/Shadowz3n/Google-Services
+import mechanize, sys, json
 
 if len(sys.argv)>2:
 	print "\033[92m[Creating Google Session]\033[0m"
@@ -29,19 +32,22 @@ if len(sys.argv)>2:
 	
 		# Get Token
 		# My paste: u/2/folders/0B2qbDxOlHLfPeThCYUFNQlQ0Wk0 or my-drive
-		br.open("https://drive.google.com/drive/u/2/folders/0B2qbDxOlHLfPeThCYUFNQlQ0Wk0")
+		br.open("https://drive.google.com/drive/u/0/folders/0B2qbDxOlHLfPeThCYUFNQlQ0Wk0")
 		token		= br.response().read().split(',[["xsrf","')[1].split('",["')[0]
 	
+		print token
+	
 		# Itens
-		itens		= br.response().read().split("window['_DRIVE_ivd'] = '")[1].replace('\\x22', '"').replace('\\n', '').replace('\\\\', '').split(";")[0].replace('\/', '/')[:-1]
+		#itens		= br.response().read().split("window['_DRIVE_ivd'] = '")[1].replace('\\x22', '"').replace('\\n', '').replace('\\\\', '').split(";")[0].replace('\/', '/')[:-1]
 	
 		# Upload file
-		br.open("https://drive.google.com/upload/resumableuploadsc?authuser=0", '{"protocolVersion":"0.8","createSessionRequest":{"fields":[{"external":{"name":"file","filename":"Seguro Fian\u00e7a - Ficha Pessoa Fisica - RESIDENCIAL.pdf","put":{},"size":188113}},{"inlined":{"name":"parentId","content":"0B6b4BiW-o9hxRlBnRVFlZzgwRVE","contentType":"text/plain"}},{"inlined":{"name":"driveSourceClientService","content":"UploadWeb","contentType":"text/plain"}},{"inlined":{"name":"modifiedTime","content":"1452096907160","contentType":"text/plain"}}]}}')
-	
-		print br.response().read()
-	
+		#br.open("https://drive.google.com/upload/resumableuploadsc?authuser=0", '{"protocolVersion":"0.8","createSessionRequest":{"fields":[{"external":{"name":"file","filename":"Seguro Fian\u00e7a - Ficha Pessoa Fisica - RESIDENCIAL.pdf","put":{},"size":188113}},{"inlined":{"name":"parentId","content":"0B6b4BiW-o9hxRlBnRVFlZzgwRVE","contentType":"text/plain"}},{"inlined":{"name":"driveSourceClientService","content":"UploadWeb","contentType":"text/plain"}},{"inlined":{"name":"modifiedTime","content":"1452096907160","contentType":"text/plain"}}]}}')
+		
 		# Download file ID
 		#br.open("https://drive.google.com/uc?id=0B6b4BiW-o9hxNk1veEJVa1FrU1E&authuser=0&export=download")
+		
+		#upload		= br.response().read()
+		#print upload
 	else:
 		print "\033[91m[The email and password you entered don't match]\033[0m"
 else:
